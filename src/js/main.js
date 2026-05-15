0c32a8e9934e7b0fdfaa51ed2dd98c4d0be973de
Dianauguerra <diana.1609@hotmail.com>
2026-05-14 18:40:56 -0600

    productlist class

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+
+// not sure if these two are ncessary
+const listElement = document.querySelector(".product-list");
+
+const productList = new ProductList("tents", dataSource, listElement);
