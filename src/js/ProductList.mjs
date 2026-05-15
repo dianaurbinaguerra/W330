8f19afc1128f9edd89a62f68df37746174510b3c
Dianauguerra <diana.1609@hotmail.com>
2026-05-14 18:40:56 -0600

    productlist class

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,7 @@
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+}
