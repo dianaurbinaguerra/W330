1fe066485256e9d9286aa478188e0d5aad5c0ff9
Dianauguerra <diana.1609@hotmail.com>
2026-05-14 18:43:31 -0600

    made a change to productList.mjs

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,7 +1,18 @@
+
 export default class ProductList {
-  constructor(category, dataSource, listElement) {
-    this.category = category;
+  constructor(dataSource) {
     this.dataSource = dataSource;
-    this.listElement = listElement;
+    this.products = [];
+  }
+
+  async init() {
+    this.products = await this.dataSource.getData();
+    console.log(this.products); // check if data loaded
+    this.render();
+  }
+
+  render() {
+    console.log("Rendering products...");
+    console.table(this.products);
   }
 }
