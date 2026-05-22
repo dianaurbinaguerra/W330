a475f64a1cc9c6b10247d3e5a68188e9bd7caa8d
emerald067 <eawoke@byupathway.edu>
2026-05-22 01:42:15 +0100

    Refactor product rendering and data fetching logic

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -8,23 +8,6 @@ function productCardTemplate(product) {
         <h2 class="card__brand">${product.Brand.Name}</h2>
         <h3 class="card__name">${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
-  const id = product.Id;
-  const name = product.NameWithoutBrand;
-  const fullName = product.Name;
-  const brand = product.Brand.Name;
-  const image = product.Image.replace("../", "/");
-  const price = product.FinalPrice || product.ListPrice;
-
-  return `
-    <li class="product-card">
-      <a href="product_pages/?product=${id}">
-        <img
-          src="${image}"
-          alt="${fullName}"
-        />
-        <h3 class="card__brand">${brand}</h3>
-        <h2 class="card__name">${name}</h2>
-        <p class="product-card__price">$${price.toFixed(2)}</p>
       </a>
     </li>
   `;
@@ -38,7 +21,6 @@ export default class ProductList {
   }
 
   async init() {
-    const list = await this.dataSource.getData(this.category);
     const list = await this.dataSource.getData();
     this.renderList(list);
   }
@@ -50,10 +32,6 @@ export default class ProductList {
       list,
       "afterbegin",
       true
-    );
-  }
-}
-      true,
     );
   }
 }
