56766d0ee2df3261c496dccf5b9a8673f599ebe9
Dianauguerra <diana.1609@hotmail.com>
2026-05-23 15:45:39 -0600

    productData.mjs completed

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
+const baseURL = import.meta.env.VITE_SERVER_URL;
 
 function convertToJson(res) {
   if (res.ok) {
@@ -9,17 +9,16 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `/json/${this.category}.json`;
+  constructor() {}
+
+  async getData(category) {
+    const response = await fetch(`${baseURL}products/search/${category}`);
+    const data = await convertToJson(response);
+    return data.Result;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
-  async findProductById(id) {
-    const products = await this.getData();
+
+  async findProductById(id, category) {
+    const products = await this.getData(category);
     return products.find((item) => item.Id === id);
   }
 }
