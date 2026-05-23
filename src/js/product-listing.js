5564bc9b0a0355a842940f31ab6ac6223e5d028e
Dianauguerra <diana.1609@hotmail.com>
2026-05-23 15:15:41 -0600

    url for tents updated in procuct-listing.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,16 +1,36 @@
+// import ProductData from "./ProductData.mjs";
+// import ProductList from "./ProductList.mjs";
+
+// import { initCartBadge } from "./utils.mjs";
+
+// const dataSource = new ProductData("tents");
+
+// const element = document.querySelector(".product-list");
+
+// const productList = new ProductList("Tents", dataSource, element);
+
+
+
+// initCartBadge();
+// productList.init();
+
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 import { initCartBadge } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
+function getCategoryFromURL() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("category") || "tents";
+}
+
+const category = getCategoryFromURL();
+
+const dataSource = new ProductData(category);
 
 const element = document.querySelector(".product-list");
 
-const productList = new ProductList("Tents", dataSource, element);
-
-
+const productList = new ProductList(category, dataSource, element);
 
 initCartBadge();
 productList.init();
-
